a57885cc592d394f0a0b3c79d32e13f0dac59a49
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-04-11 12:21:59 -0300

    Adicionei comentários explicando o código

--- ex005.js
+++ ex005.js
@@ -1,7 +1,12 @@
+
+// Vai pedir para o usuário digitar um número
 let numero = Number(prompt("Digite um número para ver o seu antecessor e sucessor: "))
+
+// Variavel que vai subitrair o valor da variavel "número" ou adicionar
 ant = numero -1
 suc = numero +1
 
+//Printando no console
 console.log(`O número sucessor do número ${numero} é ${suc} e o anterior é ${ant}`)
 
 
