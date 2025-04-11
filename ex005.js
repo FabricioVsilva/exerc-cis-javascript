e4cbb17fb496614fbb1ea6f9cf7dc011c8445800
Fabrício Vieira Da Silva <devfabriciovieira@gmail.com>
2025-04-11 10:52:40 -0300

    primeiro commit

---
+++ ex005.js
@@ -0,0 +1,10 @@
+let numero = Number(prompt("Digite um número para ver o seu antecessor e sucessor: "))
+ant = numero -1
+suc = numero +1
+
+console.log(`O número sucessor do número ${numero} é ${suc} e o anterior é ${ant}`)
+
+
+
+
+
